b9d2f668ffbf35acb9ac7bdb6140c9c1f1d596de
Blerina79 <graines.de.tomates@gmail.com>
2024-05-21 17:03:12 +0200

    ajout modale

---
+++ app/public/wp-content/themes/motaphoto/template_parts/modale.php
@@ -0,0 +1,7 @@
+
+<div id="modal" class="modal">
+    <div class="modal-content">
+        <button class="modal-off">Fermer</button>
+        <?php echo do_shortcode('[contact-form-7 id="4bee7fb" title="Contact form 1"]'); ?>
+    </div>
+</div>
